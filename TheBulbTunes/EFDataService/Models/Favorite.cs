d76056a1296b61c234fcb51e56e8640bc0a684bf
Bayowa Odometa <boyoanu@hotmail.com>
2021-08-16 13:29:29 +0100

    Define Favorite entity

---
+++ TheBulbTunes/EFDataService/Models/Favorite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBulbTunes.EFDataService.Models
+{
+   public class Favorite
+    {
+        [Required]
+        public Guid Id { get; set; }
+        
+        [Required]
+        public Guid SongId { get; set; }
+ 
+        [Required]
+        public Guid AddedByUserId { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        // The following are navigation properties made possible by the foreign-key relationship
+        public virtual Song SelectedSong { get; set; }
+        public virtual User AddedBy { get; set; }
+
+    }
+}
