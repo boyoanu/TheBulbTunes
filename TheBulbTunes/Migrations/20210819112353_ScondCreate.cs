f9e96ebaa07ce3c5cfdc10d47f4558fa164f3933
Bayowa Odometa <boyoanu@hotmail.com>
2021-08-19 14:19:42 +0100

    Updates on Migration Script

---
+++ TheBulbTunes/Migrations/20210819112353_ScondCreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheBulbTunes.Migrations
+{
+    public partial class ScondCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
