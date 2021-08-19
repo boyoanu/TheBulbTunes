e2174414bb9c0aa3d656fdf52ca6bea794b91bc7
Bayowa Odometa <boyoanu@hotmail.com>
2021-08-19 10:24:06 +0100

    Updates on the Program.cs

--- TheBulbTunes/Program.cs
+++ TheBulbTunes/Program.cs
@@ -22,7 +22,7 @@ namespace TheBulbTunes
             songService.Create("Essence", "Wizkid", "Made In Lagos", "Tems", "R&B", new DateTime(2020, 10, 30));
 
 
-            //Fetch all song
+            //Fetch all song before updates
             List<Song> availableSongs = songService.FetchAll();
             Console.WriteLine("Title\tArtist\tAlbum");
             foreach (Song song in availableSongs)
@@ -50,6 +50,53 @@ namespace TheBulbTunes
             Console.WriteLine("\n\nFILTERED SONGS FOR HOPE\n");
             Console.Write("Title\t\tArtist\t\tAlbum");
             foreach (Song song in filteredSongs2)
+            {
+                Console.WriteLine();
+                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+
+            }
+
+            // Set the id of song to be updated
+            Guid idOfSongToUpdate1 = new Guid("0fc59e9e-c4eb-43b6-3dc3-08d9625747ad"); // Non-existing id
+            Guid idOfSongToUpdate2 = new Guid("0fc59e9e-c4eb-43b6-3dc3-08d9625747ae"); // Existing id
+
+            // Create a Song object containing new info for the update
+            Song songWithNewInfo = new Song()
+            {
+                Genre = "Rap/Hip-hop",
+                ReleaseDate = new DateTime(2013, 1, 1)
+            };
+
+            // Call the Update method to make the desired update
+            songService.Update(idOfSongToUpdate1, songWithNewInfo);
+            songService.Update(idOfSongToUpdate2, songWithNewInfo);
+
+            // Fetch all songs after update
+            availableSongs = songService.FetchAll();
+
+            Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
+            Console.Write("Title\t\tArtist\t\tAlbum");
+            foreach (Song song in availableSongs)
+            {
+                Console.WriteLine();
+                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+            }
+
+
+            // Set the id of song to be deleted
+            Guid idOfSongToDelete1 = new Guid("05e4b13c-1fe2-4ef6-aa68-08d96238e7ab"); // Non-existing id
+            Guid idOfSongToDelete2 = new Guid("597b4637-92c8-4c34-aa67-08d96238e7ac"); // Existing id
+
+            // Call the Delete method to perform the desired deletion
+            songService.Delete(idOfSongToDelete1);
+            songService.Delete(idOfSongToDelete2);
+
+            // Fetch all songs after delete
+            availableSongs = songService.FetchAll();
+
+            Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
+            Console.Write("Title\t\tArtist\t\tAlbum");
+            foreach (Song song in availableSongs)
             {
                 Console.WriteLine();
                 Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
