5fc6556d408ddc403b20af021f04000bd32ebc9f
Bayowa Odometa <boyoanu@hotmail.com>
2021-08-20 16:13:39 +0100

    updates on implementation of Favourite.cs invoke in Program.cs

--- TheBulbTunes/Program.cs
+++ TheBulbTunes/Program.cs
@@ -12,95 +12,159 @@ namespace TheBulbTunes
     {
         static void Main(string[] args)
         {
-           //SongServices songService  = new SongServices();
 
-           UserServices userService = new UserServices();
+            FavouriteServices favouriteService = new FavouriteServices();
 
-            // Create a number Users
-            userService.Create("Jane", "Okeke", "jane.okeke@thebulb.africa");
-            userService.Create("Temilola", "Tegbe", "temilola.tegbe@thebulb.africa");
-            userService.Create("Adeleke", "Ayinde", "adeleke.ayinde@thebulb.africa");
-            userService.Create("Hope", "Ndudim", "hope.ndudim@thebulb.africa");
-            userService.Create("Bayowa", "odometa", "bayowa.odometa@thebulb.africa");
+            // Set the id of song to be favourited
+            Guid idOfFavouriteSong1 = new Guid("57029ed3-e5fe-467d-aa65-08d96238e7ac"); // All Over by Tiwa Savage
+            Guid idOfFavouriteSong2 = new Guid("30bd68ae-7083-4603-aa69-08d96238e7ac"); // Essence by Wizkid
+            Guid idOfFavouriteSong3 = new Guid("e58f9d01-f343-4fdb-aa66-08d96238e7ac"); // Nobody's Business by Rihanna
 
-            //Fetch all user before updates
-            List<User> availableUsers = userService.FetchAll();
-            Console.WriteLine("FirstName\tLastName\tEmailAddress");
-            foreach (User user in availableUsers)
+            // Set the id of user who wants to favourite a song
+            Guid idOfUser1 = new Guid("56624948-0ed9-4681-dffc-08d96306324f"); // Philip Amadi
+            Guid idOfUser2 = new Guid("76ffa97d-da37-42ca-dffe-08d96306324f"); // Bayowa Odometa
+
+
+            // Fetch all favourites
+            List<Favourite> availableFavourites = favouriteService.FetchAll();
+
+            Console.WriteLine("\n\nCURRENTLY AVAILABLE FAVOURITES:\n");
+            Console.Write("Title\t\tArtist\t\tFavourited By");
+            foreach (Favourite favourite in availableFavourites)
             {
                 Console.WriteLine();
-                Console.WriteLine($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+                Console.Write($"{favourite.SelectedSong.Title}\t{favourite.SelectedSong.Artist}\t{favourite.AddedBy.FirstName} {favourite.AddedBy.LastName}");
             }
 
-            // Fetch users that meet some search criteria
-            List<User> filteredUsers1 = userService.FetchWithFilter("Ja","Ok",".africa");
-            List<User> filteredUsers2 = userService.FetchWithFilter("Te","be","@theBulb");
-            List<User> filteredUsers3 = userService.FetchWithFilter();
-            List<User> filteredUsers4 = userService.FetchWithFilter();
+            // Invoke the Create method of FavouriteService to mark some songs as favourites for some users
+            favouriteService.Create(idOfFavouriteSong1, idOfUser1);
+            favouriteService.Create(idOfFavouriteSong2, idOfUser2);
+            favouriteService.Create(idOfFavouriteSong3, idOfUser2);
 
 
-            Console.WriteLine("\n\nFILTERED USERS FOR JANE\n");
-            Console.Write("FirstName\t\tLastName\t\tEmailAddress");
-            foreach (User user in filteredUsers1)
+            // Fetch all favourites
+            availableFavourites = favouriteService.FetchAll();
+
+            Console.WriteLine("\n\nCURRENTLY AVAILABLE FAVOURITES:\n");
+            Console.Write("Title\t\tArtist\t\tFavourited By");
+            foreach (Favourite favourite in availableFavourites)
             {
                 Console.WriteLine();
-                Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
-            }
-
-            Console.WriteLine("\n\nFILTERED USERS FOR HOPE\n");
-            Console.Write("FirstName\t\tLastName\t\tEmailAddress");
-            foreach (User user in filteredUsers2)
-            {
-                Console.WriteLine();
-                Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
-
-            }
-
-            // Set the id of song to be updated
-            Guid idOfUserToUpdate1 = new Guid("0fc59e9e-c4eb-43b6-3dc3-08d9625747ad"); // Non-existing id
-            Guid idOfUserToUpdate2 = new Guid("0fc59e9e-c4eb-43b6-3dc3-08d9625747ae"); // Existing id
-
-            // Create a Song object containing new info for the update
-            User userWithNewInfo = new User()
-            {
-                FirstName = "",
-                EmailAddress = ""
-            };
-
-            // Call the Update method to make the desired update
-            userService.Update(idOfUserToUpdate1, userWithNewInfo);
-            userService.Update(idOfUserToUpdate2, userWithNewInfo);
-
-            // Fetch all songs after update
-            availableUsers = userService.FetchAll();
-
-            Console.WriteLine("\n\nCURRENTLY AVAILABLE USERS:\n");
-            Console.Write("FirstName\t\tLastName\t\tEmailAddress");
-            foreach (User user in availableUsers)
-            {
-                Console.WriteLine();
-                Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+                Console.Write($"{favourite.SelectedSong.Title}\t{favourite.SelectedSong.Artist}\t{favourite.AddedBy.FirstName} {favourite.AddedBy.LastName}");
             }
 
 
-            // Set the id of song to be deleted
-            Guid idOfUserToDelete1 = new Guid("05e4b13c-1fe2-4ef6-aa68-08d96238e7ab"); // Non-existing id
-            Guid idOfUserToDelete2 = new Guid("597b4637-92c8-4c34-aa67-08d96238e7ac"); // Existing id
+            // Declare filter parameters
+            string artistFilter, titleFilter, userFilter;
 
-            // Call the Delete method to perform the desired deletion
-            userService.Delete(idOfUserToDelete1);
-            userService.Delete(idOfUserToDelete2);
+            // Assign values to the filter parameters
+            artistFilter = "Wizkid";
+            userFilter = "Bayowa";
+            titleFilter = null;
 
-            // Fetch all songs after delete
-            availableUsers = userService.FetchAll();
+            // Invoke the FetchWithFilter method of FavouriteService to retrieve favourites that meet the filter criteria
+            List<Favourite> filteredFavourites = favouriteService.FetchWithFilter(titleFilter, userFilter, artistFilter);
 
-            Console.WriteLine("\n\nCURRENTLY AVAILABLE USERS:\n");
-            Console.Write("FirstName\t\tLastName\t\tEmailAddress");
-            foreach (User user in availableUsers)
+            Console.WriteLine("\n\nFILTERED FAVOURITES:\n");
+            Console.Write("Title\t\tArtist\t\tFavourited By");
+            foreach (Favourite favourite in filteredFavourites)
             {
                 Console.WriteLine();
-                Console.Write($"{user.LastName}\t{user.LastName}\t{user.EmailAddress}");
+                Console.Write($"{favourite.SelectedSong.Title}\t{favourite.SelectedSong.Artist}\t{favourite.AddedBy.FirstName} {favourite.AddedBy.LastName}");
             }
+
+
+        }
+
+        ////SongServices songService  = new SongServices();
+
+        //UserServices userService = new UserServices();
+
+        //    // Create a number Users
+        //    userService.Create("Jane", "Okeke", "jane.okeke@thebulb.africa");
+        //    userService.Create("Temilola", "Tegbe", "temilola.tegbe@thebulb.africa");
+        //    userService.Create("Adeleke", "Ayinde", "adeleke.ayinde@thebulb.africa");
+        //    userService.Create("Hope", "Ndudim", "hope.ndudim@thebulb.africa");
+        //    userService.Create("Bayowa", "odometa", "bayowa.odometa@thebulb.africa");
+
+        //    //Fetch all user before updates
+        //    List<User> availableUsers = userService.FetchAll();
+        //    Console.WriteLine("FirstName\tLastName\tEmailAddress");
+        //    foreach (User user in availableUsers)
+        //    {
+        //        Console.WriteLine();
+        //        Console.WriteLine($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+        //    }
+
+        //    // Fetch users that meet some search criteria
+        //    List<User> filteredUsers1 = userService.FetchWithFilter("Ja","Ok",".africa");
+        //    List<User> filteredUsers2 = userService.FetchWithFilter("Te","be","@theBulb");
+        //    List<User> filteredUsers3 = userService.FetchWithFilter();
+        //    List<User> filteredUsers4 = userService.FetchWithFilter();
+
+
+        //    Console.WriteLine("\n\nFILTERED USERS FOR JANE\n");
+        //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
+        //    foreach (User user in filteredUsers1)
+        //    {
+        //        Console.WriteLine();
+        //        Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+        //    }
+
+        //    Console.WriteLine("\n\nFILTERED USERS FOR HOPE\n");
+        //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
+        //    foreach (User user in filteredUsers2)
+        //    {
+        //        Console.WriteLine();
+        //        Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+
+        //    }
+
+        //    // Set the id of user to be updated
+        //    Guid idOfUserToUpdate1 = new Guid("05719cae-7258-48dd-2ada-08d96310aaf4"); // Non-existing id
+        //    Guid idOfUserToUpdate2 = new Guid("05719cae-7258-48dd-2ada-08d96310aaf3"); // Existing id
+
+        //    // Create a User object containing new info for the update
+        //    User userWithNewInfo = new User()
+        //    {
+        //        FirstName = "Bayowa",
+        //        EmailAddress = "bayowa.odometa@thebulb.africa"
+        //    };
+
+        //    // Call the Update method to make the desired update
+        //    userService.Update(idOfUserToUpdate1, userWithNewInfo);
+        //    userService.Update(idOfUserToUpdate2, userWithNewInfo);
+
+        //    // Fetch all users after update
+        //    availableUsers = userService.FetchAll();
+
+        //    Console.WriteLine("\n\nCURRENTLY AVAILABLE USERS:\n");
+        //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
+        //    foreach (User user in availableUsers)
+        //    {
+        //        Console.WriteLine();
+        //        Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+        //    }
+
+
+        //    // Set the id of user to be deleted
+        //    Guid idOfUserToDelete1 = new Guid("05e4b13c-1fe2-4ef6-aa68-08d96238e7ab"); // Non-existing id
+        //    Guid idOfUserToDelete2 = new Guid("597b4637-92c8-4c34-aa67-08d96238e7ac"); // Existing id
+
+        //    // Call the Delete method to perform the desired deletion
+        //    userService.Delete(idOfUserToDelete1);
+        //    userService.Delete(idOfUserToDelete2);
+
+        //    // Fetch all users after delete
+        //    availableUsers = userService.FetchAll();
+
+        //    Console.WriteLine("\n\nCURRENTLY AVAILABLE USERS:\n");
+        //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
+        //    foreach (User user in availableUsers)
+        //    {
+        //        Console.WriteLine();
+        //        Console.Write($"{user.LastName}\t{user.LastName}\t{user.EmailAddress}");
+        //    }
 
             // Create Number of Songs
             //songService.Create("All Over", "Tiwa Savage", "Sugarcane", "", "Afro-pop,Romantic", new DateTime(2017, 5, 22));
@@ -110,14 +174,14 @@ namespace TheBulbTunes
             //songService.Create("Essence", "Wizkid", "Made In Lagos", "Tems", "R&B", new DateTime(2020, 10, 30));
 
 
-            //Fetch all song before updates
-            List<Song> availableSongs = songService.FetchAll();
-            Console.WriteLine("Title\tArtist\tAlbum");
-            foreach (Song song in availableSongs)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"{song.Title}\t{song.Artist}\t{song.Album}");
-            }
+            ////Fetch all song before updates
+            //List<Song> availableSongs = songService.FetchAll();
+            //Console.WriteLine("Title\tArtist\tAlbum");
+            //foreach (Song song in availableSongs)
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine($"{song.Title}\t{song.Artist}\t{song.Album}");
+            //}
 
 
             //// Fetch songs that meet some search criteria
@@ -127,72 +191,72 @@ namespace TheBulbTunes
             //List<Song> filteredSongs4 = songService.FetchWithFilter();
 
 
-            Console.WriteLine("\n\nFILTERED SONGS FOR JANE\n");
-            Console.Write("Title\t\tArtist\t\tAlbum");
-            foreach (Song song in filteredSongs1)
-            {
-                Console.WriteLine();
-                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
-            }
+            //Console.WriteLine("\n\nFILTERED SONGS FOR JANE\n");
+            //Console.Write("Title\t\tArtist\t\tAlbum");
+            //foreach (Song song in filteredSongs1)
+            //{
+            //    Console.WriteLine();
+            //    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+            //}
 
-            Console.WriteLine("\n\nFILTERED SONGS FOR HOPE\n");
-            Console.Write("Title\t\tArtist\t\tAlbum");
-            foreach (Song song in filteredSongs2)
-            {
-                Console.WriteLine();
-                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+            //Console.WriteLine("\n\nFILTERED SONGS FOR HOPE\n");
+            //Console.Write("Title\t\tArtist\t\tAlbum");
+            //foreach (Song song in filteredSongs2)
+            //{
+            //    Console.WriteLine();
+            //    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
 
-            }
+            //}
 
             // Set the id of song to be updated
-            Guid idOfSongToUpdate1 = new Guid("0fc59e9e-c4eb-43b6-3dc3-08d9625747ad"); // Non-existing id
-            Guid idOfSongToUpdate2 = new Guid("0fc59e9e-c4eb-43b6-3dc3-08d9625747ae"); // Existing id
+            //Guid idOfSongToUpdate1 = new Guid("0fc59e9e-c4eb-43b6-3dc3-08d9625747ad"); // Non-existing id
+            //Guid idOfSongToUpdate2 = new Guid("0fc59e9e-c4eb-43b6-3dc3-08d9625747ae"); // Existing id
 
-            // Create a Song object containing new info for the update
-            Song songWithNewInfo = new Song()
-            {
-                Genre = "Rap/Hip-hop",
-                ReleaseDate = new DateTime(2013, 1, 1)
-            };
+            //// Create a Song object containing new info for the update
+            //Song songWithNewInfo = new Song()
+            //{
+            //    Genre = "Rap/Hip-hop",
+            //    ReleaseDate = new DateTime(2013, 1, 1)
+            //};
 
-            // Call the Update method to make the desired update
-            songService.Update(idOfSongToUpdate1, songWithNewInfo);
-            songService.Update(idOfSongToUpdate2, songWithNewInfo);
+            //// Call the Update method to make the desired update
+            //songService.Update(idOfSongToUpdate1, songWithNewInfo);
+            //songService.Update(idOfSongToUpdate2, songWithNewInfo);
 
-            // Fetch all songs after update
-            availableSongs = songService.FetchAll();
+            //// Fetch all songs after update
+            //availableSongs = songService.FetchAll();
 
-            Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
-            Console.Write("Title\t\tArtist\t\tAlbum");
-            foreach (Song song in availableSongs)
-            {
-                Console.WriteLine();
-                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
-            }
+            //Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
+            //Console.Write("Title\t\tArtist\t\tAlbum");
+            //foreach (Song song in availableSongs)
+            //{
+            //    Console.WriteLine();
+            //    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+            //}
 
 
-            // Set the id of song to be deleted
-            Guid idOfSongToDelete1 = new Guid("05e4b13c-1fe2-4ef6-aa68-08d96238e7ab"); // Non-existing id
-            Guid idOfSongToDelete2 = new Guid("597b4637-92c8-4c34-aa67-08d96238e7ac"); // Existing id
+            //// Set the id of song to be deleted
+            //Guid idOfSongToDelete1 = new Guid("05e4b13c-1fe2-4ef6-aa68-08d96238e7ab"); // Non-existing id
+            //Guid idOfSongToDelete2 = new Guid("597b4637-92c8-4c34-aa67-08d96238e7ac"); // Existing id
 
-            // Call the Delete method to perform the desired deletion
-            songService.Delete(idOfSongToDelete1);
-            songService.Delete(idOfSongToDelete2);
+            //// Call the Delete method to perform the desired deletion
+            //songService.Delete(idOfSongToDelete1);
+            //songService.Delete(idOfSongToDelete2);
 
-            // Fetch all songs after delete
-            availableSongs = songService.FetchAll();
+            //// Fetch all songs after delete
+            //availableSongs = songService.FetchAll();
 
-            Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
-            Console.Write("Title\t\tArtist\t\tAlbum");
-            foreach (Song song in availableSongs)
-            {
-                Console.WriteLine();
-                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
-            }
+            //Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
+            //Console.Write("Title\t\tArtist\t\tAlbum");
+            //foreach (Song song in availableSongs)
+            //{
+            //    Console.WriteLine();
+            //    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+            //}
 
 
         }
 
     }
-    }
+    
 
