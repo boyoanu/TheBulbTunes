f83487a30753eb18235232f6d8378d75dad87245
Bayowa Odometa <boyoanu@hotmail.com>
2021-08-20 16:11:33 +0100

    updates on the immplementation of UserServices.cs

--- TheBulbTunes/EFDataService/Services/UserServices.cs
+++ TheBulbTunes/EFDataService/Services/UserServices.cs
@@ -11,11 +11,8 @@ namespace TheBulbTunes.EFDataService.Services
     {
 
         private readonly TheBulbTunesContext _context = new TheBulbTunesContext();
-        private List<User> _users = new List<User> { };
-        
-        
-        
-
+        private List<User> _users = new List<User>{ };
+  
         //Create a user
         public void Create(string firstName, string lastName, string emailAddress)
         {
@@ -38,7 +35,7 @@ namespace TheBulbTunes.EFDataService.Services
         //Fetch users with filter(FirstName, LastName, EmailAddress,)
         public List<User> FetchWithFilter(string firstNameFilter = null, string lastNameFilter = null, string emailAddressFilter = null)
         {
-            // Retrieve all available songs unfiltered
+            // Retrieve all available users unfiltered
             _users = FetchAll();
 
             // If any filter is specified, apply that filter by calling its search method
@@ -46,6 +43,7 @@ namespace TheBulbTunes.EFDataService.Services
             if (firstNameFilter != null)
                 _users = SearchByFirstName(firstNameFilter, _users);
 
+           
             if (lastNameFilter != null)
                 _users = SearchByLastName(lastNameFilter, _users);
 
@@ -57,26 +55,27 @@ namespace TheBulbTunes.EFDataService.Services
 
         private List<User> SearchByFirstName(string searchValue, List<User> users)
         {
-            return users.Where(s => s.FirstName.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+            return users.Where(u => u.FirstName.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
         }
+
 
         private List<User> SearchByLastName(string searchValue, List<User> users)
         {
-            return users.Where(s => s.LastName.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+            return users.Where(u => u.LastName.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
         }
 
         private List<User> SearchByEmailAddress(string searchValue, List<User> users)
         {
-            return users.Where(s => s.EmailAddress.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+            return users.Where(u => u.EmailAddress.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
         }
 
-        //Update song
+        //Update user
 
-        public void Update(Guid id, User userWithNewInfo)
+        public void Update(Guid id, User userUpdateInfo)
         {
-            // Check if a song with the supplied id exists
+            // Check if a user with the supplied id exists
             User userToUpdate = FetchAll()
-                .Where(s => s.UserId == id)
+                .Where(u => u.UserId == id)
                 .FirstOrDefault();
 
             if (userToUpdate == null)
@@ -85,31 +84,31 @@ namespace TheBulbTunes.EFDataService.Services
                 return;
             }
 
-            // A matching song was found. Perform the requested update.
-            if (userWithNewInfo.FirstName != null) userToUpdate.FirstName = userWithNewInfo.FirstName;
-            if (userWithNewInfo.LastName != null) userToUpdate.LastName = userWithNewInfo.LastName;
-            if (userWithNewInfo.EmailAddress != null) userToUpdate.EmailAddress = userWithNewInfo.EmailAddress;
+            // A matching user was found. Perform the requested update.
+            if (userUpdateInfo.FirstName != null) userToUpdate.FirstName = userUpdateInfo.FirstName;
+            if (userUpdateInfo.LastName != null) userToUpdate.LastName = userUpdateInfo.LastName;
+            if (userUpdateInfo.EmailAddress != null) userToUpdate.EmailAddress = userUpdateInfo.EmailAddress;
             
             _context.SaveChanges();
         }
 
-        //Delete song
+        //Delete user
 
-        public void Delete(Guid id)
+        public void UserInfoDelete(Guid id)
         {
-            // Check if a song with the supplied id exists
-            User userToDelete = FetchAll()
-                .Where(s => s.UserId == id)
+            // Check if a user with the supplied id exists
+            User userInfoToDelete = FetchAll()
+                .Where(u => u.UserId == id)
                 .FirstOrDefault();
 
-            if (userToDelete == null)
+            if (userInfoToDelete == null)
             {
                 Console.WriteLine($"Invalid operation! No match was found for the id you supplied.");
                 return;
             }
 
-            // A matching song was found. Perform the requested deletion.
-            _context.Users.Remove(userToDelete);
+            // A matching  was found. Perform the requested deletion.
+            _context.Users.Remove(userInfoToDelete);
             _context.SaveChanges();
         }
     }
