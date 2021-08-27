c9146d9da105d1f7b42cf1b34b57e0d6883a54db
Bayowa Odometa <boyoanu@hotmail.com>
2021-08-27 16:00:18 +0100

    Updates on invoking program.cs

--- TheBulbTunes/Program.cs
+++ TheBulbTunes/Program.cs
@@ -16,13 +16,13 @@ namespace TheBulbTunes
             FavouriteServices favouriteService = new FavouriteServices();
 
             // Set the id of song to be favourited
-            Guid idOfFavouriteSong1 = new Guid("57029ed3-e5fe-467d-aa65-08d96238e7ac"); // All Over by Tiwa Savage
-            Guid idOfFavouriteSong2 = new Guid("30bd68ae-7083-4603-aa69-08d96238e7ac"); // Essence by Wizkid
-            Guid idOfFavouriteSong3 = new Guid("e58f9d01-f343-4fdb-aa66-08d96238e7ac"); // Nobody's Business by Rihanna
+            Guid idOfFavouriteSong1 = new Guid("99912f4e-9349-4e1e-3dc0-08d9625747ae"); // All Over by Tiwa Savage
+            Guid idOfFavouriteSong2 = new Guid("7ab62546-2045-4ebb-3dc4-08d9625747ae"); // Essence by Wizkid
+            Guid idOfFavouriteSong3 = new Guid("172f62b3-d445-4168-3dc1-08d9625747ae"); // Nobody's Business by Rihanna
 
             // Set the id of user who wants to favourite a song
-            Guid idOfUser1 = new Guid("56624948-0ed9-4681-dffc-08d96306324f"); // Philip Amadi
-            Guid idOfUser2 = new Guid("76ffa97d-da37-42ca-dffe-08d96306324f"); // Bayowa Odometa
+            Guid idOfUser1 = new Guid("2359e7a7-5e07-4440-2ad9-08d96310aaf3"); // Hope Ndudim
+            Guid idOfUser2 = new Guid("05719cae-7258-48dd-2ada-08d96310aaf3"); // Bayowa Odometa
 
 
             // Fetch all favourites
@@ -42,129 +42,131 @@ namespace TheBulbTunes
             favouriteService.Create(idOfFavouriteSong3, idOfUser2);
 
 
-            // Fetch all favourites
-            availableFavourites = favouriteService.FetchAll();
+            //// Fetch all favourites
+            //availableFavourites = favouriteService.FetchAll();
 
-            Console.WriteLine("\n\nCURRENTLY AVAILABLE FAVOURITES:\n");
-            Console.Write("Title\t\tArtist\t\tFavourited By");
-            foreach (Favourite favourite in availableFavourites)
-            {
-                Console.WriteLine();
-                Console.Write($"{favourite.SelectedSong.Title}\t{favourite.SelectedSong.Artist}\t{favourite.AddedBy.FirstName} {favourite.AddedBy.LastName}");
-            }
-
-
-            // Declare filter parameters
-            string artistFilter, titleFilter, userFilter;
-
-            // Assign values to the filter parameters
-            artistFilter = "Wizkid";
-            userFilter = "Bayowa";
-            titleFilter = null;
-
-            // Invoke the FetchWithFilter method of FavouriteService to retrieve favourites that meet the filter criteria
-            List<Favourite> filteredFavourites = favouriteService.FetchWithFilter(titleFilter, userFilter, artistFilter);
-
-            Console.WriteLine("\n\nFILTERED FAVOURITES:\n");
-            Console.Write("Title\t\tArtist\t\tFavourited By");
-            foreach (Favourite favourite in filteredFavourites)
-            {
-                Console.WriteLine();
-                Console.Write($"{favourite.SelectedSong.Title}\t{favourite.SelectedSong.Artist}\t{favourite.AddedBy.FirstName} {favourite.AddedBy.LastName}");
-            }
+            //Console.WriteLine("\n\nCURRENTLY AVAILABLE FAVOURITES:\n");
+            //Console.Write("Title\t\tArtist\t\tFavourited By");
+            //foreach (Favourite favourite in availableFavourites)
+            //{
+            //    Console.WriteLine();
+            //    Console.Write($"{favourite.SelectedSong.Title}\t{favourite.SelectedSong.Artist}\t{favourite.AddedBy.FirstName} {favourite.AddedBy.LastName}");
+            //}
 
 
-        }
+            //    // Declare filter parameters
+            //    string artistFilter, titleFilter, userFilter;
 
-        ////SongServices songService  = new SongServices();
+            //    // Assign values to the filter parameters
+            //    artistFilter = "Wizkid";
+            //    userFilter = "Bayowa";
+            //    titleFilter = null;
 
-        //UserServices userService = new UserServices();
+            //    // Invoke the FetchWithFilter method of FavouriteService to retrieve favourites that meet the filter criteria
+            //    List<Favourite> filteredFavourites = favouriteService.FetchWithFilter(titleFilter, userFilter, artistFilter);
 
-        //    // Create a number Users
-        //    userService.Create("Jane", "Okeke", "jane.okeke@thebulb.africa");
-        //    userService.Create("Temilola", "Tegbe", "temilola.tegbe@thebulb.africa");
-        //    userService.Create("Adeleke", "Ayinde", "adeleke.ayinde@thebulb.africa");
-        //    userService.Create("Hope", "Ndudim", "hope.ndudim@thebulb.africa");
-        //    userService.Create("Bayowa", "odometa", "bayowa.odometa@thebulb.africa");
-
-        //    //Fetch all user before updates
-        //    List<User> availableUsers = userService.FetchAll();
-        //    Console.WriteLine("FirstName\tLastName\tEmailAddress");
-        //    foreach (User user in availableUsers)
-        //    {
-        //        Console.WriteLine();
-        //        Console.WriteLine($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
-        //    }
-
-        //    // Fetch users that meet some search criteria
-        //    List<User> filteredUsers1 = userService.FetchWithFilter("Ja","Ok",".africa");
-        //    List<User> filteredUsers2 = userService.FetchWithFilter("Te","be","@theBulb");
-        //    List<User> filteredUsers3 = userService.FetchWithFilter();
-        //    List<User> filteredUsers4 = userService.FetchWithFilter();
+            //    Console.WriteLine("\n\nFILTERED FAVOURITES:\n");
+            //    Console.Write("Title\t\tArtist\t\tFavourited By");
+            //    foreach (Favourite favourite in filteredFavourites)
+            //    {
+            //        Console.WriteLine();
+            //        Console.Write($"{favourite.SelectedSong.Title}\t{favourite.SelectedSong.Artist}\t{favourite.AddedBy.FirstName} {favourite.AddedBy.LastName}");
+            //    }
 
 
-        //    Console.WriteLine("\n\nFILTERED USERS FOR JANE\n");
-        //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
-        //    foreach (User user in filteredUsers1)
-        //    {
-        //        Console.WriteLine();
-        //        Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
-        //    }
-
-        //    Console.WriteLine("\n\nFILTERED USERS FOR HOPE\n");
-        //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
-        //    foreach (User user in filteredUsers2)
-        //    {
-        //        Console.WriteLine();
-        //        Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
-
-        //    }
-
-        //    // Set the id of user to be updated
-        //    Guid idOfUserToUpdate1 = new Guid("05719cae-7258-48dd-2ada-08d96310aaf4"); // Non-existing id
-        //    Guid idOfUserToUpdate2 = new Guid("05719cae-7258-48dd-2ada-08d96310aaf3"); // Existing id
-
-        //    // Create a User object containing new info for the update
-        //    User userWithNewInfo = new User()
-        //    {
-        //        FirstName = "Bayowa",
-        //        EmailAddress = "bayowa.odometa@thebulb.africa"
-        //    };
-
-        //    // Call the Update method to make the desired update
-        //    userService.Update(idOfUserToUpdate1, userWithNewInfo);
-        //    userService.Update(idOfUserToUpdate2, userWithNewInfo);
-
-        //    // Fetch all users after update
-        //    availableUsers = userService.FetchAll();
-
-        //    Console.WriteLine("\n\nCURRENTLY AVAILABLE USERS:\n");
-        //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
-        //    foreach (User user in availableUsers)
-        //    {
-        //        Console.WriteLine();
-        //        Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
-        //    }
+            //}
 
 
-        //    // Set the id of user to be deleted
-        //    Guid idOfUserToDelete1 = new Guid("05e4b13c-1fe2-4ef6-aa68-08d96238e7ab"); // Non-existing id
-        //    Guid idOfUserToDelete2 = new Guid("597b4637-92c8-4c34-aa67-08d96238e7ac"); // Existing id
 
-        //    // Call the Delete method to perform the desired deletion
-        //    userService.Delete(idOfUserToDelete1);
-        //    userService.Delete(idOfUserToDelete2);
+            //UserServices userService = new UserServices();
 
-        //    // Fetch all users after delete
-        //    availableUsers = userService.FetchAll();
+            //    // Create a number Users
+            //    userService.Create("Jane", "Okeke", "jane.okeke@thebulb.africa");
+            //    userService.Create("Temilola", "Tegbe", "temilola.tegbe@thebulb.africa");
+            //    userService.Create("Adeleke", "Ayinde", "adeleke.ayinde@thebulb.africa");
+            //    userService.Create("Hope", "Ndudim", "hope.ndudim@thebulb.africa");
+            //    userService.Create("Bayowa", "odometa", "bayowa.odometa@thebulb.africa");
 
-        //    Console.WriteLine("\n\nCURRENTLY AVAILABLE USERS:\n");
-        //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
-        //    foreach (User user in availableUsers)
-        //    {
-        //        Console.WriteLine();
-        //        Console.Write($"{user.LastName}\t{user.LastName}\t{user.EmailAddress}");
-        //    }
+            //    //Fetch all user before updates
+            //    List<User> availableUsers = userService.FetchAll();
+            //    Console.WriteLine("FirstName\tLastName\tEmailAddress");
+            //    foreach (User user in availableUsers)
+            //    {
+            //        Console.WriteLine();
+            //        Console.WriteLine($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+            //    }
+
+            //    // Fetch users that meet some search criteria
+            //    List<User> filteredUsers1 = userService.FetchWithFilter("Ja","Ok",".africa");
+            //    List<User> filteredUsers2 = userService.FetchWithFilter("Te","be","@theBulb");
+            //    List<User> filteredUsers3 = userService.FetchWithFilter();
+            //    List<User> filteredUsers4 = userService.FetchWithFilter();
+
+
+            //    Console.WriteLine("\n\nFILTERED USERS FOR JANE\n");
+            //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
+            //    foreach (User user in filteredUsers1)
+            //    {
+            //        Console.WriteLine();
+            //        Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+            //    }
+
+            //    Console.WriteLine("\n\nFILTERED USERS FOR HOPE\n");
+            //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
+            //    foreach (User user in filteredUsers2)
+            //    {
+            //        Console.WriteLine();
+            //        Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+
+            //    }
+
+            //    // Set the id of user to be updated
+            //    Guid idOfUserToUpdate1 = new Guid("05719cae-7258-48dd-2ada-08d96310aaf4"); // Non-existing id
+            //    Guid idOfUserToUpdate2 = new Guid("05719cae-7258-48dd-2ada-08d96310aaf3"); // Existing id
+
+            //    // Create a User object containing new info for the update
+            //    User userWithNewInfo = new User()
+            //    {
+            //        FirstName = "Bayowa",
+            //        EmailAddress = "bayowa.odometa@thebulb.africa"
+            //    };
+
+            //    // Call the Update method to make the desired update
+            //    userService.Update(idOfUserToUpdate1, userWithNewInfo);
+            //    userService.Update(idOfUserToUpdate2, userWithNewInfo);
+
+            //    // Fetch all users after update
+            //    availableUsers = userService.FetchAll();
+
+            //    Console.WriteLine("\n\nCURRENTLY AVAILABLE USERS:\n");
+            //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
+            //    foreach (User user in availableUsers)
+            //    {
+            //        Console.WriteLine();
+            //        Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
+            //    }
+
+
+            //    // Set the id of user to be deleted
+            //    Guid idOfUserToDelete1 = new Guid("05e4b13c-1fe2-4ef6-aa68-08d96238e7ab"); // Non-existing id
+            //    Guid idOfUserToDelete2 = new Guid("597b4637-92c8-4c34-aa67-08d96238e7ac"); // Existing id
+
+            //    // Call the Delete method to perform the desired deletion
+            //    userService.Delete(idOfUserToDelete1);
+            //    userService.Delete(idOfUserToDelete2);
+
+            //    // Fetch all users after delete
+            //    availableUsers = userService.FetchAll();
+
+            //    Console.WriteLine("\n\nCURRENTLY AVAILABLE USERS:\n");
+            //    Console.Write("FirstName\t\tLastName\t\tEmailAddress");
+            //    foreach (User user in availableUsers)
+            //    {
+            //        Console.WriteLine();
+            //        Console.Write($"{user.LastName}\t{user.LastName}\t{user.EmailAddress}");
+            //    }
+
+            ////SongServices songService  = new SongServices();
 
             // Create Number of Songs
             //songService.Create("All Over", "Tiwa Savage", "Sugarcane", "", "Afro-pop,Romantic", new DateTime(2017, 5, 22));
@@ -258,5 +260,6 @@ namespace TheBulbTunes
         }
 
     }
+}
     
 
